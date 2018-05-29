603583528171b1f3883dd3410309ca12aae11e98
Andranik Antonyan <andantonyan@gmail.com>
2018-05-29 06:13:20 +0400

    feat(interface): geometry

--- interface/index.ts
+++ interface/index.ts
@@ -1,3 +1,26 @@
+import { GeometryType } from '../enum';
+
 export interface JwtPayload {
   id: number;
+}
+
+export interface Geometry {
+  type: GeometryType;
+  coordinates: number[];
+}
+
+export interface Pin {
+  lat: number;
+  lng: number;
+  title?: string;
+  icon?: {
+    path: string;
+    width: number;
+    height: number;
+  };
+  infoWindow?: {
+    maxWidth?: number;
+    contentFn(pin: Pin): string;
+  };
+  meta?: any;
 }
