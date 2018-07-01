15caaa96d9db5f620ca07a0fa2e7b95075539eae
Andranik Antonyan <andantonyan@gmail.com>
2018-07-01 17:08:30 +0400

    chore(Organization): empty OrganizationCreateRequestDto

--- dto/organization/index.ts
+++ dto/organization/index.ts
@@ -1,4 +1,5 @@
 export * from './branch.dto';
 export * from './organization.dto';
+export * from './organization-create-request.dto';
 export * from './organization-list-response.dto';
 export * from './organization-list-query-request.dto';
