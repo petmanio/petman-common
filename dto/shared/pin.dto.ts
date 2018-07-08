24e849ec6a40ad95a9928553cc4aacd992a85f6d
Andranik Antonyan <andantonyan@gmail.com>
2018-07-08 15:56:21 +0400

    chore(Pin): add images

--- dto/shared/pin.dto.ts
+++ dto/shared/pin.dto.ts
@@ -1,5 +1,6 @@
 import { ApiModelProperty } from '@nestjs/swagger';
-import { Type } from 'class-transformer';
+import { Transform, Type } from 'class-transformer';
+import { map } from 'lodash';
 
 import { dtoGetter } from '../../lib';
 import { AddressDto } from './address.dto';
@@ -14,6 +15,10 @@ export class PinDto {
 
   @ApiModelProperty({ type: String })
   description: string;
+
+  @ApiModelProperty({ type: String, isArray: true })
+  @Transform(images => map(images, image => '/upload' + image), { groups: ['petman-api'] })
+  images: string[];
 
   @ApiModelProperty({ type: AddressDto })
   @Type(dtoGetter(AddressDto))
